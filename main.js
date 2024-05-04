9df79030396ea473fa50aebf5566897f59bd1f32
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-05-04 20:36:22 +0700

    refactor: fake data js

---
+++ main.js
@@ -0,0 +1,100 @@
+// ________FAKE_DATA_______________
+let questions = [
+  {
+    quiz_id: 1,
+    question:
+      "You can learn a lot about the local _______ by talking to local people.",
+    answers: ["territory", "area", "land", "nation"],
+  },
+  {
+    quiz_id: 2,
+    question:
+      "It's good to have someone to ________ you when you are visiting a new place.",
+    answers: ["lead", "take", "guide", "bring"],
+  },
+  {
+    quiz_id: 3,
+    question:
+      "When you ______ your destination, your tour guide will meet you at the airport.",
+    answers: ["arrive", "reach", "get", "achieve"],
+  },
+  {
+    quiz_id: 4,
+    question: "It can be quite busy here during the tourist ______",
+    answers: ["season", "phase", "period", "stage"],
+  },
+  {
+    quiz_id: 5,
+    question:
+      "Make sure you _______ a hotel before you come to our island, especially in the summer.",
+    answers: ["book", "keep", "put", "buy"],
+  },
+  {
+    quiz_id: 6,
+    question: "Captain Cook discovered Australia on a _______ to the Pacific.",
+    answers: ["vacation", "travel", "cruise", "voyage"],
+  },
+  {
+    quiz_id: 7,
+    question:
+      " Most tourist attractions in London charge an admission ________.",
+    answers: ["fare", "ticket", "fee", "pay"],
+  },
+  {
+    quiz_id: 8,
+    question: "The hotel where we are _______ is quite luxurious.",
+    answers: ["living", "existing", "remaining", "staying"],
+  },
+  {
+    quiz_id: 9,
+    question: "Is English an ________ language in your country?",
+    answers: ["mother", "official", "living", "old"],
+  },
+  {
+    quiz_id: 10,
+    question: "He spoke a ______ of French that we found hard to understand.",
+    answers: ["slang", "jargon", "dialect", "language"],
+  },
+];
+const results = [
+  {
+    quiz_id: 1,
+    answer: "area",
+  },
+  {
+    quiz_id: 3,
+    answer: "reach",
+  },
+  {
+    quiz_id: 2,
+    answer: "guide",
+  },
+  {
+    quiz_id: 4,
+    answer: "season",
+  },
+  {
+    quiz_id: 5,
+    answer: "book",
+  },
+  {
+    quiz_id: 6,
+    answer: "voyage",
+  },
+  {
+    quiz_id: 7,
+    answer: "fee",
+  },
+  {
+    quiz_id: 8,
+    answer: "staying",
+  },
+  {
+    quiz_id: 9,
+    answer: "official",
+  },
+  {
+    quiz_id: 10,
+    answer: "dialect",
+  },
+];
